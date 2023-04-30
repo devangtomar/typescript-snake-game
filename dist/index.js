880f6bc0229703a8e42381f73a6fde3a6bb22b6d
Devang Tomar <devangtomar123@gmail.com>
2023-04-30 22:47:17 +0530

    ✨ Adding Snake game!

--- dist/index.js
+++ dist/index.js
@@ -1,34 +1,30 @@
-interface Point {
-    x: number;
-    y: number;
-}
-
-type Direction = 'up' | 'down' | 'left' | 'right';
-
-class Snake {
-    private readonly ctx : CanvasRenderingContext2D;
-    private readonly canvasSize : number;
-    private readonly blockSize : number;
-    private readonly moveInterval : number;
-
-    private direction : Direction = 'right';
-    private snake : Point[] = [];
-    private food : Point = {
-        x: 0,
-        y: 0
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
     };
-    private moveTimeout : NodeJS.Timeout | null = null;
-
-    constructor(ctx : CanvasRenderingContext2D, canvasSize : number, blockSize : number, moveInterval : number) {
+    return __assign.apply(this, arguments);
+};
+var Snake = /** @class */ (function () {
+    function Snake(ctx, canvasSize, blockSize, moveInterval) {
+        this.direction = 'right';
+        this.snake = [];
+        this.food = {
+            x: 0,
+            y: 0
+        };
+        this.moveTimeout = null;
         this.ctx = ctx;
         this.canvasSize = canvasSize;
         this.blockSize = blockSize;
         this.moveInterval = moveInterval;
-
         this.reset();
     }
-
-    private reset(): void { // Initialize the snake with three segments
+    Snake.prototype.reset = function () {
         this.snake = [
             {
                 x: 2,
@@ -41,81 +37,74 @@ class Snake {
                 y: 0
             },
         ];
-
         // Generate the first food location
         this.generateFood();
-
         // Set the initial direction to right
         this.direction = 'right';
-    }
-
-    private generateFood(): void { // Generate a random point within the canvas bounds
+    };
+    Snake.prototype.generateFood = function () {
         this.food = {
             x: Math.floor(Math.random() * (this.canvasSize / this.blockSize)),
             y: Math.floor(Math.random() * (this.canvasSize / this.blockSize))
         };
-    }
-
-    private drawBlock(point : Point, color : string): void {
-        const x = point.x * this.blockSize;
-        const y = point.y * this.blockSize;
+    };
+    Snake.prototype.drawBlock = function (point, color) {
+        var x = point.x * this.blockSize;
+        var y = point.y * this.blockSize;
         this.drawRect(x, y, this.blockSize, this.blockSize, color);
-    }
-
-    private drawRect(x : number, y : number, width : number, height : number, color : string): void {
+    };
+    Snake.prototype.drawRect = function (x, y, width, height, color) {
         this.ctx.fillStyle = color;
         this.ctx.fillRect(x, y, width, height);
-    }
-
-    private move(): void { // Move the snake by adding a new head segment and removing the tail segment
-        const head = {
-            ...this.snake[0]
-        };
+    };
+    Snake.prototype.move = function () {
+        var _this = this;
+        var head = __assign({}, this.snake[0]);
         switch (this.direction) {
-            case 'up': head.y -= 1;
+            case 'up':
+                head.y -= 1;
                 break;
-            case 'down': head.y += 1;
+            case 'down':
+                head.y += 1;
                 break;
-            case 'left': head.x -= 1;
+            case 'left':
+                head.x -= 1;
                 break;
-            case 'right': head.x += 1;
+            case 'right':
+                head.x += 1;
                 break;
         }
         this.snake.unshift(head);
         this.snake.pop();
-
         // Check for collision with the food
         if (this.snake[0].x === this.food.x && this.snake[0].y === this.food.y) {
             this.snake.push(this.snake[this.snake.length - 1]);
             this.generateFood();
         }
-
         // Check for collision with the walls or snake body
-        const [headX, headY] = [
+        var _a = [
             this.snake[0].x,
             this.snake[0].y
-        ];
+        ], headX = _a[0], headY = _a[1];
         if (headX < 0 || headX >= this.canvasSize / this.blockSize || headY < 0 || headY >= this.canvasSize / this.blockSize) {
             this.reset();
             return;
         }
-        for (let i = 1; i < this.snake.length; i++) {
+        for (var i = 1; i < this.snake.length; i++) {
             if (this.snake[i].x === headX && this.snake[i].y === headY) {
                 this.reset();
                 return;
             }
         }
-
         // Draw
         this.ctx.clearRect(0, 0, this.canvasSize, this.canvasSize);
         this.drawBlock(this.food, 'red');
-        this.snake.forEach((point, i) => {
-            const color = i === 0 ? 'black' : 'green';
-            this.drawBlock(point, color);
+        this.snake.forEach(function (point, i) {
+            var color = i === 0 ? 'black' : 'green';
+            _this.drawBlock(point, color);
         });
-    }
-
-    private handleKeyDown(event : KeyboardEvent): void { // Update the direction based on the arrow key pressed
+    };
+    Snake.prototype.handleKeyDown = function (event) {
         switch (event.key) {
             case 'ArrowUp':
                 if (this.direction !== 'down') {
@@ -138,31 +127,29 @@ class Snake {
                 }
                 break;
         }
-    }
-    public start(): void {
-        this.moveTimeout = setInterval(() => this.move(), this.moveInterval);
-        window.addEventListener('keydown', (event) => this.handleKeyDown(event));
-    }
-
-    public stop(): void {
+    };
+    Snake.prototype.start = function () {
+        var _this = this;
+        this.moveTimeout = setInterval(function () { return _this.move(); }, this.moveInterval);
+        window.addEventListener('keydown', function (event) { return _this.handleKeyDown(event); });
+    };
+    Snake.prototype.stop = function () {
+        var _this = this;
         if (this.moveTimeout) {
             clearInterval(this.moveTimeout);
         }
-        window.removeEventListener('keydown', (event) => this.handleKeyDown(event));
-    }
-}
-
+        window.removeEventListener('keydown', function (event) { return _this.handleKeyDown(event); });
+    };
+    return Snake;
+}());
 // Initialize the canvas and snake objects
-const canvas = document.getElementById('canvas')as HTMLCanvasElement;
-const ctx = canvas.getContext('2d')as CanvasRenderingContext2D;
-const canvasSize = 400;
-const blockSize = 10;
-const moveInterval = 100;
-
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var canvasSize = 400;
+var blockSize = 10;
+var moveInterval = 100;
 canvas.width = canvasSize;
 canvas.height = canvasSize;
-
-const snake = new Snake(ctx, canvasSize, blockSize, moveInterval);
-
+var snake = new Snake(ctx, canvasSize, blockSize, moveInterval);
 // Start the game
 snake.start();
